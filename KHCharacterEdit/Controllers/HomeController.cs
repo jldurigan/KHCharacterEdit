ca3ed6638bf56bc51ac6f81b282be21ac9ce68f1
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2022-06-16 16:30:07 -0300

    Commit inicial

---
+++ KHCharacterEdit/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KHCharacterEdit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Customize your favorite characters from this beloved series!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Entre em contato com o desenvolvedor:";
+
+            return View();
+        }
+    }
+}
